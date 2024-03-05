e9ee86c781e3c9c8052facd8367e938bb8f78d2a
BorisIurciuc <149237585+BorisIurciuc@users.noreply.github.com>
2024-03-05 18:05:53 +0100

    add cod plant.js hw10

--- homework_10/plant.js
+++ homework_10/plant.js
@@ -1,1 +1,20 @@
+class Plant {
+    constructor(height, age) {
+        this.height = height;
+        this.age = age;
+    }
+    grow() {
+        this.height = this.height + 10;
+    }
+}
 
+class Rose extends Plant {
+    constructor(height, age, numberOfFlowers){
+        super(height, age);
+        this.numberOfFlowers = numberOfFlowers;
+    }
+}
+const rose = new Rose(15, 3, 36);
+rose.grow();
+
+console.log(rose);
