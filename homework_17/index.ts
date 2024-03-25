780fc57bfcd89d9cd511f9fc1865e64434e288a8
BorisIurciuc <149237585+BorisIurciuc@users.noreply.github.com>
2024-03-25 09:32:29 +0100

    Update hw17 index.ts

--- homework_17/index.ts
+++ homework_17/index.ts
@@ -1,1 +1,32 @@
+interface Airplane {
+    numberOfEngines: number;
+    isJet: string;
+    maxHeight: number;
+    capacity: number;
+}
+const boing777: Airplane = {
+    numberOfEngines: 2,
+    isJet: "турбовентиляторный ",
+    maxHeight: 13140,
+    capacity: 365,
+}
+const a320: Airplane = {
+    numberOfEngines: 2,
+    isJet: "турбовентиляторный",
+    maxHeight: 12000,
+    capacity: 180,
+}
+const l410: Airplane = {
+    numberOfEngines: 2,
+    isJet: "турбовинтовой",
+    maxHeight: 4100,
+    capacity: 19,
+}
+console.log(boing777);
+console.log(a320);
+console.log(l410);
+
+
+
+
 
